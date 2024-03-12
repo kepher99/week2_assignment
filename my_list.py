39bd15f69c5cb0060770bd46266b186fe9ae0bfd
kepher99 <otienokepher9@gmail.com>
2024-03-13 00:20:35 +0300

    my_list assignment

---
+++ my_list.py
@@ -0,0 +1,10 @@
+my_list = [10,20,30,40]
+my_list[1]=15
+my_list.extend([50,60,70])
+my_list.pop()
+my_list.sort()
+
+#print(my_list)
+
+index_of_30 = my_list.index(30)
+print("Index of 30 in my_list:", index_of_30)
